899c6339de78cf29b6d5726b8334bdbc9761b066
Cameron Gray <cameron.gray@senecacollege.ca>
2024-12-17 11:21:12 -0500

    2251-Update

--- ts/main.ts
+++ ts/main.ts
@@ -13,7 +13,7 @@
 //
 // Data Version: update this when the data file changes to ensure the data 
 // is reloaded on clients side
-const dv = "20241212.4";
+const dv = "20241217.1";
 const lastUpdated = "2024-12-17";
 var activeTerm = "2251";
 
@@ -507,7 +507,7 @@ function processFaculty()
                         section.style.backgroundColor = legendList[idx].backgroundColor;
                         section.style.borderColor = legendList[idx].backgroundColor;
                         section.style.color = legendList[idx].color;
-                        section.innerHTML = rec.subject + rec.catalog + ' (' + rec.section + ')<br />' + rec.first[0] + '. ' + rec.last + ' (Enrl:' + rec.totEnrol + ')<br />' + rec.room;
+                        section.innerHTML = rec.subject + rec.catalog + ' (' + rec.section + ')<br />' + rec.first[0] + '. ' + rec.last + ' (enrl:' + rec.totEnrol + ')<br />' + rec.room;
                         section.title = rec.subject + rec.catalog + ':' + rec.section + '\n' + rec.first[0] + '.' + rec.last + '\n' + rec.room + '\n' + rec.timeStart + '-' + rec.timeEnd + '\n' + rec.campus;
                         
                         // Add the data cell to the schedule table
@@ -613,7 +613,7 @@ function processCourse()
                         id:rec.subject + rec.catalog + rec.section.substring(0, 3) + rec.first[0] + '. ' + rec.last,
                         backgroundColor: colour.background,
                         color: colour.color,
-                        innerHTML: rec.subject.substring(0, 3) + rec.catalog + ' - ' + rec.section.substring(0, 3) + ' (Enrl:' + rec.totEnrol +
+                        innerHTML: rec.subject.substring(0, 3) + rec.catalog + ' - ' + rec.section.substring(0, 3) + ' (enrl:' + rec.totEnrol +
                                     ')<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(' + rec.last + ', ' + rec.first + ')',
                         contactHours: 0
                     };
@@ -746,7 +746,7 @@ function processRoom()
                         id: rec.room + rec.subject + rec.catalog + rec.section.substring(0, 3) + rec.first[0] + '. ' + rec.last,
                         backgroundColor: colour.background,
                         color: colour.color,
-                        innerHTML: rec.room + ' : ' + rec.subject.substring(0, 3) + rec.catalog + ' - ' + rec.section.substring(0, 3) + '(' + rec.totEnrol +
+                        innerHTML: rec.room + ' : ' + rec.subject.substring(0, 3) + rec.catalog + ' - ' + rec.section.substring(0, 3) + '(enrl:' + rec.totEnrol +
                                     ')<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + rec.last + ', ' + rec.first,
                         contactHours: 0
                     };
