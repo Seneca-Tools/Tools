ac83e622678132134a338bf22c738a2df3f49841
Cameron Gray <cameron.gray@senecapolytechnic.ca>
2026-05-07 14:37:24 -0400

    Data Updated

--- ts/main.ts
+++ ts/main.ts
@@ -13,8 +13,8 @@
 //
 // Data Version: update this when the data file changes to ensure the data 
 // is reloaded on clients side
-const dv = "20260501.1";
-const lastUpdated = "2026-05-01";
+const dv = "20260507.1";
+const lastUpdated = "2026-05-07";
 var activeTerm = "2264";
 
 // Data File: CSV file containing the data to be used by the app (https://seneca-tools.github.io/Tools/assets)
